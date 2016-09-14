762ccd6095cdc337f384b949ee198865b7876263
Fang Jian <fangjianmail@126.com>
2016-09-14 16:16:29 +0800

    mk son ok

--- src/server/tree-nedb/index.js
+++ src/server/tree-nedb/index.js
@@ -4,9 +4,9 @@ var Promise = require('bluebird');
 
 
 var db;
-function tree_nedb(_db){
+function tree_nedb(_db,cb){
   db=Promise.promisifyAll(_db);
-  buildRootIfNotExist(db);
+  buildRootIfNotExist(cb);
   return {
     read_node,
     read_nodes,
@@ -23,28 +23,82 @@ function tree_nedb(_db){
 
 module.exports=tree_nedb;
 
-function buildRootIfNotExist(){
+function buildRootIfNotExist(cb){
   return async(function(){
     var root=await(db.findOneAsync({_id:'0'}));
-    console.log(root)
-    return {hello:'world'}
+    if(!root){
+      var defaultRoot={
+        _id:'0', 
+        _link: {
+          p: '0',
+          children: []
+        }
+      };
+      root=await(db.insertAsync(defaultRoot));
+    }
+    if(typeof cb =='function'){
+      cb(); //通知root构建完成
+    }
+    return root;
   })();
 }
 
 function read_node(gid) {
-
+  return async(function(){
+    var root=await(db.findOneAsync({_id:gid}));
+    return root;
+  })();
 }
 
 function read_nodes(gids) {
-
+  return async(function(){
+    var root=await(db.findOneAsync({_id:{$in:gids}}));
+    return root;
+  })();
 }
 
-function mk_son_by_data(gid, data) {
-
+function _mk_son_by_data(pNode,data,bgid){
+  return async(function(){
+    console.log(pNode);
+    var newNode={
+        _link: {
+          p: pNode._id,
+          children: []
+        },
+        _data:data
+    };
+    var _newNode= await(db.insertAsync(newNode));//插入新节点
+    var pos=0;
+    var children=pNode._link.children;
+    if(bgid){
+      pos=children.indexOf(bgid)+1;
+    }
+    children.splice(pos,0,_newNode._id);//把新节点的ID插入到父节点中
+    await(db.updateAsync({_id:pNode._id}, pNode, {}));//插入父节点
+    return _newNode;//返回新节点
+  })();
 }
 
-function mk_brother_by_data(data, bgid) {
+function mk_son_by_data(pgid, data) {
+  return async(function(){
+    var pNode=await(db.findOneAsync({"_id":pgid}));//找到父节点
+    if(!pNode){
+      throw ('cannot find parent node '+pgid);
+      return null;//父节点不存在，无法插入，返回null
+    }
+    return _mk_son_by_data(pNode,data);
+  })();
+}
 
+function mk_brother_by_data(bgid,data) {
+  return async(function(){
+    var pNode=await(db.findOneAsync({"_link.children":{$elemMatch:bgid}}));//找到父节点
+    if(!pNode){
+      throw ('cannot find parent node of brother '+bgid);
+      return null;//父节点不存在，无法插入，返回null
+    }
+    return _mk_son_by_data(pNode,data,bgid);
+  })();
 }
 
 function update_data(gid, data) {
--- test/tree-nedb/smoke.js
+++ test/tree-nedb/smoke.js
@@ -1,7 +1,8 @@
 var assert = require("assert");
 var nedb = require('nedb');
 var treeDb = new nedb({ filename: 'tree.nedb', autoload: true });
-var tree=require('../../src/server/tree-nedb')(treeDb);
+// var tree=require('../../src/server/tree-nedb')(treeDb);
+var tree;
 var async = require('asyncawait/async');
 var await = require('asyncawait/await');
 
@@ -9,17 +10,40 @@ describe('tree', function(){
 
   before(function(done){
     treeDb.remove({}, { multi: true }, function (err, numRemoved) {
-      done();
+      tree=require('../../src/server/tree-nedb')(treeDb,done);
     });
   })
 
-  it('should buildRootIfNotExist ', function(done){
+  // it('should buildRootIfNotExist ', function(done){
+  //     async(function(){
+  //       var root= await(tree.buildRootIfNotExist());
+  //       assert.equal(typeof root,'object');
+  //       assert.equal(root._id,'0');
+  //       console.log(root)
+  //       done();
+  //   })()
+  // })
+
+  it('should mk_son_by_data ', function(done){
       async(function(){
-        var root= await(tree.buildRootIfNotExist());
-        assert.equal(typeof root,'object');
-        console.log(root)
+        var son= await(tree.mk_son_by_data('0','hello'));
+        assert.equal(typeof son,'object');
+        console.log(son)
         done();
     })()
-  })
+  });
+
+  // it('should mk_brother_by_data ', function(done){
+  //     async(function(){
+  //       var son= await(tree.mk_son_by_data('0','hello'));
+  //       assert.equal(typeof son,'object');
+  //       console.log('son',son)
+  //       var bro= await(tree.mk_brother_by_data(son._id,'world'));
+  //       assert.equal(typeof bro,'object');
+  //       console.log('bro',bro)
+  //       done();
+  //   })()
+  // });
+
 
 });
