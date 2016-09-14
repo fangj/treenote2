596520eb6eafa7d960a2447f89b0ff648bcf8271
Fang Jian <fangjianmail@126.com>
2016-09-14 14:42:26 +0800

    mocha test with async await

--- src/server/tree-nedb/index.js
+++ src/server/tree-nedb/index.js
@@ -1,5 +1,64 @@
-function tree_nedb(){
-  return "ok";
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
+var Promise = require('bluebird');
+
+
+var db;
+function tree_nedb(_db){
+  db=Promise.promisifyAll(_db);
+  buildRootIfNotExist(db);
+  return {
+    read_node,
+    read_nodes,
+    mk_son_by_data,
+    mk_brother_by_data,
+    update_data,
+    remove,
+    move_as_son,
+    move_as_brother,
+    //for test
+    buildRootIfNotExist
+  }
 }
 
 module.exports=tree_nedb;
+
+function buildRootIfNotExist(){
+  return async(function(){
+    var root=await(db.findOneAsync({_id:'0'}));
+    console.log(root)
+    return {hello:'world'}
+  })();
+}
+
+function read_node(gid) {
+
+}
+
+function read_nodes(gids) {
+
+}
+
+function mk_son_by_data(gid, data) {
+
+}
+
+function mk_brother_by_data(data, bgid) {
+
+}
+
+function update_data(gid, data) {
+
+}
+
+function remove(gid) {
+
+}
+
+function move_as_son(gid, pgid) {
+
+}
+
+function move_as_brother(gid, bgid) {
+
+}
--- test/tree-nedb/smoke.js
+++ test/tree-nedb/smoke.js
@@ -1,10 +1,25 @@
 var assert = require("assert");
-var tree=require('../../src/server/tree-nedb');
+var nedb = require('nedb');
+var treeDb = new nedb({ filename: 'tree.nedb', autoload: true });
+var tree=require('../../src/server/tree-nedb')(treeDb);
+var async = require('asyncawait/async');
+var await = require('asyncawait/await');
 
 describe('tree', function(){
 
-  it('should be function', function(){
-      assert.equal('function',typeof tree);
+  before(function(done){
+    treeDb.remove({}, { multi: true }, function (err, numRemoved) {
+      done();
+    });
+  })
+
+  it('should buildRootIfNotExist ', function(done){
+      async(function(){
+        var root= await(tree.buildRootIfNotExist());
+        assert.equal(typeof root,'object');
+        console.log(root)
+        done();
+    })()
   })
 
 });
